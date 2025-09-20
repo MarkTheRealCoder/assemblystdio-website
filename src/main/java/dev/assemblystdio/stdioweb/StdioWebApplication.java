28dd5ab6235ac8760c186bdfecbe20622ccc3ff7
MRC_CODE <marcolacivita400@gmail.com>
2025-09-20 11:20:52 +0200

    initialize Spring Boot application with basic structure and configuration

---
+++ src/main/java/dev/assemblystdio/stdioweb/StdioWebApplication.java
@@ -0,0 +1,13 @@
+package dev.assemblystdio.stdioweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdioWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StdioWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/assemblystdio/stdioweb/StdioWebApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.assemblystdio.stdioweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdioWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
