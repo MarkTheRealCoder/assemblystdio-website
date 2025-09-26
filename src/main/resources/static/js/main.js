471fd2a8a95be3cc9a3ec10e4e959983f809e801
MRC_CODE <marcolacivita400@gmail.com>
2025-09-26 22:03:09 +0200

    add examples page with navigation and content sections, enhance home page layout with summary cards

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -4,3 +4,18 @@ function scrollDown(elementId) {
         element.scrollIntoView({ behavior: 'smooth' });
     }
 }
+
+function setMainContent(cid) {
+    const ids = ['ijvm', 'a8088', 'style'];
+    for (const id of ids) {
+        let itsActive = id === cid;
+        const element = document.getElementById(id);
+        element.style.display = (itsActive) ? 'flex' : 'none';
+        if (itsActive) {
+            document.getElementById(`switch-${cid}`).classList.add('active');
+        } else {
+            document.getElementById(`switch-${id}`).classList.remove('active');
+        }
+    }
+
+}
